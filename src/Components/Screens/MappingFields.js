c382a8f85aa1f94726c461071e1e15c202bab5c7
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-08-14 21:05:10 +0530

    code cleanup

--- src/Components/Screens/MappingFields.js
+++ src/Components/Screens/MappingFields.js
@@ -1,7 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 export default function MappingFields(props) {
-
     const {
         onMappingChange,
         onSequenceChange, 
