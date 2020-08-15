5140fcb684b56a14f36f158c485ced9c00597dee
yogesh8177 <yogesh.jadhav8177@gmail.com>
2020-08-15 08:35:40 +0530

    code cleanup

--- src/Components/Screens/FormatSelect.js
+++ src/Components/Screens/FormatSelect.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 export default function FormatSelect(props) {
     const {onFormatChange, currentScreenIndex, screenIndex, outputformat} = props;
